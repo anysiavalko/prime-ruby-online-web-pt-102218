f514fa6b145eef1a317a128327d0a1f2015c51dc
A. V <anysiavalko@gmail.com>
2018-11-06 02:28:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,4 +7,4 @@ def prime?(number)
     return false
   end
 end
-#The method takes a number and divides it by all the numbers that come before it to see if there is a remainder. All the results have to show a remainder for it to be prime. If there is a remainder for all numbers, it will evaluate to true 
+#The method takes a number and divides it by all the numbers that come before it to see if there is a remainder. All the results have to show a remainder for it to be prime. If there is a remainder for all numbers, it will return true.
